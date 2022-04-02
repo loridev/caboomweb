a4d7953f50973664807ed25d6c8aa4065f1e3480
loridev <79363993+loridev@users.noreply.github.com>
2022-04-02 13:36:01 +0200

    fix3?

--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -9,4 +9,9 @@ use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 class PersonalAccessToken extends SanctumPersonalAccessToken
 {
     protected $connection = 'mysql';
+
+    public $incrementing = true;
+
+    protected $primaryKey = "id";
+    protected $keyType = "string";
 }
